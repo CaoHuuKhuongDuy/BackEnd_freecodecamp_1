953d3d3ece4aabdfbd9bdb783f6b30845c1a2606
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2022-08-31 14:19:35 +0700

    update

--- index.js
+++ index.js
@@ -24,9 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/2015-12-25",function(req,res){
+  let time = new Date().toString();
+  // let time = "ssdsdd";
+  res.send(time);
+})
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
